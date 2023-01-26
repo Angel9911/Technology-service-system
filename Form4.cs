ba610b2003faf6ca95e2803a5581f36c17cb4e16
unknown <angel_dimitrov99@abv.bg>
2023-01-26 18:16:08 +0200

    First commit

---
+++ Form4.cs
@@ -0,0 +1,442 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example2
+{
+    public partial class Form4 : Form
+    {
+        // public static String lblSearch;
+        private static string oradb = "User Id=MARKET;Password=a123;Data Source=DESKTOP-O1SSE52/XE";
+        public static String rdBtnPrice;
+        public static String checkBtnBrand;
+        public static int checkBoxAscending = 0;
+        public static int chechBoxDescending = 0;
+        public static int chechBoxNews = 0;
+        private String search;
+        private int id_Client;
+        private int id_App;
+        private int id_Office;
+        private int id_Brand;
+        private int id_Delivery;
+        private int id_Price;
+
+        public Form4(string lblSearch)
+        {
+            InitializeComponent();
+            searchLbl.Text = lblSearch;
+        }
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            panel3.Height = 65;
+            comboBox1.Items.Add("Цени Възходящо");
+            comboBox1.Items.Add("Цени Низходящо");
+            comboBox1.Items.Add("Нови");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "Цени Възходящо")
+            {
+                checkBoxAscending = 1;
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Цени Низходящо")
+            {
+                chechBoxDescending = -1;
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Нови")
+            {
+                chechBoxNews = 2;
+            }
+        }
+        private int getIdFromClient(string NameClient) // id client za poruchki
+        {
+            string oradb = "User Id=MARKET;Password=a123;Data Source=DESKTOP-O1SSE52/XE";
+            OracleConnection conn = new OracleConnection(oradb);
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText="SELECT C.ID_CLIENT \n"+
+                "FROM CLIENT C \n"+
+                "WHERE C.NAME_CLIENT='"+ NameClient + "';";
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                conn.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    id_Client = (int)reader["ID_CLIENT"];
+                }
+                reader.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return id_Client;
+        }
+        private int getIdFromAppliance(string NameAppliance)// id appliance za poruchki
+        {
+            string oradb = "User Id=MARKET;Password=a123;Data Source=DESKTOP-O1SSE52/XE";
+            OracleConnection conn = new OracleConnection(oradb);
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT A.ID_APPLIANCE \n" +
+                "FROM TYPE_OF_APPLIANCES A \n" +
+                "A.NAME_APPLIANCE='" + NameAppliance + "'";
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                conn.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    id_App = (int)reader["ID_APPLIANCE"];
+                }
+                reader.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return id_App;
+        }
+        private int getIdFromBrand(string NameBrand)// id brand za poruchki
+        {
+            string oradb = "User Id=MARKET;Password=a123;Data Source=DESKTOP-O1SSE52/XE";
+            OracleConnection conn = new OracleConnection(oradb);
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT B.ID_BRAND \n" +
+                "FROM TYPE_OF_BRANDS B \n" +
+                "B.NAME_BRAND='" + NameBrand + "'";
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                conn.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    id_Brand = (int)reader["ID_BRAND"];
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return id_Brand;
+        }
+        private int getIdFromOffice(string city,string address)// id office za poruchki
+        {
+            string oradb = "User Id=MARKET;Password=a123;Data Source=DESKTOP-O1SSE52/XE";
+            OracleConnection conn = new OracleConnection(oradb);
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT O.ID_OFFICE \n" +
+                "FROM OFFICE O \n" +
+                "O.LOCATION_CITY='" + city + "' AND O.ADDRESS_OFFICE='"+ address + "'";
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                conn.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    id_Office = (int)reader["ID_OFFICE"];
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return id_Office;
+        }
+        private int getIdFromPrice(double price)
+        {
+            string oradb = "User Id=MARKET;Password=a123;Data Source=DESKTOP-O1SSE52/XE";
+            OracleConnection conn = new OracleConnection(oradb);
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT P.ID_PRICE \n" +
+                "FROM PRICE P \n" +
+                "P.PRICE='" + price + "';";
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                conn.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    id_Price = (int)reader["ID_PRICE"];
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return id_Price;
+        }
+        private int getIdFromDelivery(string deliv_to,string date)// date? ? ? Tuka trqbva da se vidi da se dobavi i eventualno id na client pri poruchkata zashtoto
+            // moje da ima dve poruchki s edin i sushti grad i data
+        {
+            string oradb = "User Id=MARKET;Password=a123;Data Source=DESKTOP-O1SSE52/XE";
+            OracleConnection conn = new OracleConnection(oradb);
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT D.ID_DELIVERY \n" +
+                "FROM DELIVERY D \n" +
+                "D.DELIVERY_TO='" + deliv_to + "' AND D.DATE_SENDING='" + date + "'";
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                conn.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    id_Delivery = (int)reader["ID_DELIVERY"];
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return id_Delivery;
+        }
+        private String SelectAfterFilter(String price,String brands,String priceRange)
+        {
+            int br = 0;
+            string firstRnage,secondRange;
+            firstRnage = priceRange.Replace("-", " ");
+            secondRange = priceRange.Replace("-", " ");
+            int len = firstRnage.Length;
+            len = firstRnage.IndexOf(" ");
+            firstRnage = firstRnage.Remove(len);// tuk trqbva da izleze purvata chast
+            len = firstRnage.Length;
+            secondRange = secondRange.Remove(0, len);//a tyk vtorata
+            if(price.Equals("Цени Възходящо"))
+            {
+                br = 1;
+            }
+            else if(price.Equals("Цени Низходящо"))
+            {
+                br = -1;
+            } // tova e za vyzhodqshta i nizhodqshta cena
+            String sqlQuery = null;
+            OracleConnection conn = new OracleConnection(oradb);
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            if (br == 1)
+            {
+                cmd.CommandText = "SELECT A.NAME_APPLIANCE,B.NAME_BRAND,P.PRICE \n" +
+                    "FROM TYPE_OF_APPLIANCES A \n" +
+                    "INNER JOIN ORDERS O ON O.APPLIANCE_ID_APPLIANCE=A.ID_APPLIANCE \n" +
+                    "INNER JOIN TYPE_OF_BRANDS B ON O.BRAND_ID_BRAND=B.ID_BRAND \n" +
+                    "INNER JOIN PRICE P ON O.PRICE_ID_PRICE=P.ID_PRICE \n" +
+                    "WHERE P.PRICE BETWEEN '" + firstRnage + "'AND'" + secondRange + "' \n" +
+                    "ORDER BY P.PRICE ASC";
+                cmd.CommandType = CommandType.Text;
+            }
+            else if (br < 0)
+            {
+                cmd.CommandText = "SELECT A.NAME_APPLIANCE,B.NAME_BRAND,P.PRICE \n" +
+                   "FROM TYPE_OF_APPLIANCES A \n" +
+                   "INNER JOIN ORDERS O ON O.APPLIANCE_ID_APPLIANCE=A.ID_APPLIANCE \n" +
+                   "INNER JOIN TYPE_OF_BRANDS B ON O.BRAND_ID_BRAND=B.ID_BRAND \n" +
+                   "INNER JOIN PRICE P ON O.PRICE_ID_PRICE=P.ID_PRICE \n" +
+                   "WHERE P.PRICE BETWEEN '" + firstRnage + "'AND'" + secondRange + "' \n" +
+                   "ORDER BY P.PRICE DESC";
+                cmd.CommandType = CommandType.Text;
+            }
+            try
+            {
+                conn.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    sqlQuery = (string)reader["ID_CONSULTANT"];
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return sqlQuery;
+        }
+        private void FilterBtn_Click(object sender, EventArgs e)
+        {
+            this.Text = rdBtnPrice;
+            label4.Text = checkBtnBrand;
+            if(chechBoxDescending!=0 && checkBoxAscending==0 && chechBoxNews == 0)
+            {
+                label5.Text = "Цени Низходящо";
+            }
+            else if(checkBoxAscending!=0 && chechBoxDescending==0 && chechBoxNews == 0)
+            {
+                label5.Text = "Цени Възходящо";
+            }
+            else
+            {
+                label5.Text = "Нови";
+            }
+            LGChckBox.Checked = false;
+            CrownChckBox.Checked = false;
+            GorenjeChckBox.Checked = false;
+            checkBtnBrand = "";
+            checkBoxAscending = 0;
+            chechBoxDescending = 0;
+            chechBoxNews = 0;
+    }
+
+        private void radiobtn1_200_CheckedChanged(object sender, EventArgs e)
+        {
+            rdBtnPrice = "1-200";
+        }
+
+        private void radiobtn200_400_CheckedChanged(object sender, EventArgs e)
+        {
+            rdBtnPrice = "200-400";
+        }
+
+        private void radiobtn400_800_CheckedChanged(object sender, EventArgs e)
+        {
+            rdBtnPrice = "400-800";
+        }
+
+        private void LGChckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+            checkBtnBrand += "LG ";
+        }
+
+        private void CrownChckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+            checkBtnBrand += "Crown ";
+
+        }
+
+        private void GorenjeChckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+            checkBtnBrand += "Gorenje ";
+        }
+
+        private void menuBtn_Click(object sender, EventArgs e)
+        {
+            if (panel3.Height == 252)
+            {
+                panel3.Height = 65;
+
+            }
+            else
+            {
+                panel3.Height = 252;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            this.Hide();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem.Text == "Контакти")
+            {
+                Form5 f5 = new Form5();
+                f5.Show();
+                this.Hide();
+            }
+            else if (e.ClickedItem.Text == "Магазини")
+            {
+                Shops shops = new Shops();
+                shops.Show();
+                this.Hide();
+            }
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            search = srchBox.Text;
+            string query_appliance = null;
+            string query_brand = null;
+            string query_price = null;
+            OracleConnection conn = new OracleConnection(oradb);
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT A.NAME_APPLIANCE,B.NAME_BRAND,P.PRICE \n" +
+                "FROM TYPE_OF_APPLIANCES A \n" +
+                "INNER JOIN ORDERS O ON O.APPLIANCE_ID_APPLIANCE=A.ID_APPLIANCE \n" +
+                "INNER JOIN TYPE_OF_BRANDS B ON O.BRAND_ID_BRAND=B.ID_BRAND \n" +
+                "INNER JOIN PRICE P ON O.PRICE_ID_PRICE=P.ID_PRICE \n" +
+                "WHERE B.NAME_BRAND='" + search + "' \n";
+            cmd.CommandType = CommandType.Text;
+            /*
+             try
+            {
+                conn.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    query_appliance = (string)reader["NAME_APPLIANCE"];
+                    query_brand = (string)reader["NAME_BRAND"];
+                    query_price = (string)reader["PRICE"];
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+             */
+        }
+    }
+}
---
+++ ShopsTable.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Example2
+{
+    partial class ShopsTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.shop = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.workTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.to = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.work = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.shop,
+            this.address,
+            this.workTime,
+            this.to,
+            this.work});
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.Snow;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.GridColor = System.Drawing.Color.LightGray;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(988, 458);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            this.dataGridView1.ColumnWidthChanged += new System.Windows.Forms.DataGridViewColumnEventHandler(this.dataGridView1_ColumnWidthChanged);
+            this.dataGridView1.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView1_RowsAdded);
+            this.dataGridView1.Paint += new System.Windows.Forms.PaintEventHandler(this.dataGridView1_Paint);
+            // 
+            // shop
+            // 
+            this.shop.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.shop.DataPropertyName = "LOCATION_CITY";
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            this.shop.DefaultCellStyle = dataGridViewCellStyle2;
+            this.shop.HeaderText = "Магазин";
+            this.shop.Name = "shop";
+            this.shop.Width = 79;
+            // 
+            // address
+            // 
+            this.address.DataPropertyName = "ADDRESS_OFFICE";
+            this.address.HeaderText = "Адрес";
+            this.address.Name = "address";
+            this.address.Width = 210;
+            // 
+            // workTime
+            // 
+            this.workTime.DataPropertyName = "OPEN";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle3.Format = "T";
+            dataGridViewCellStyle3.NullValue = null;
+            this.workTime.DefaultCellStyle = dataGridViewCellStyle3;
+            this.workTime.HeaderText = "От";
+            this.workTime.Name = "workTime";
+            this.workTime.Width = 160;
+            // 
+            // to
+            // 
+            this.to.DataPropertyName = "CLOSE";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle4.Format = "T";
+            dataGridViewCellStyle4.NullValue = null;
+            this.to.DefaultCellStyle = dataGridViewCellStyle4;
+            this.to.HeaderText = "До";
+            this.to.Name = "to";
+            this.to.Width = 200;
+            // 
+            // work
+            // 
+            this.work.DataPropertyName = "NAME";
+            this.work.HeaderText = "Продавач-консултант";
+            this.work.Name = "work";
+            this.work.Width = 296;
+            // 
+            // backBtn
+            // 
+            this.backBtn.BackColor = System.Drawing.Color.Red;
+            this.backBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.backBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.backBtn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.backBtn.ForeColor = System.Drawing.Color.White;
+            this.backBtn.Location = new System.Drawing.Point(336, 515);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(125, 36);
+            this.backBtn.TabIndex = 16;
+            this.backBtn.Text = "Назад";
+            this.backBtn.UseVisualStyleBackColor = false;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(988, 45);
+            this.panel1.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Списък от магазини";
+            // 
+            // ShopsTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1012, 563);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.backBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ShopsTable";
+            this.Text = "ShopsTable";
+            this.Load += new System.EventHandler(this.ShopsTable_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn shop;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn workTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn to;
+        private System.Windows.Forms.DataGridViewTextBoxColumn work;
+    }
+}
